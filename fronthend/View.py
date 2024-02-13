158daa94fe123a47e8c69108b8de5eae1c7e6cc3
Kamal Agrahari <kamalagrahari2003@gmail.com>
2024-02-13 20:11:20 +0530

    added UploadFile.py

---
+++ fronthend/View.py
@@ -0,0 +1,14 @@
+
+"""
+This is where we implement our UI using tkinter
+This file has the View class
+"""
+import constants as const
+
+
+class View:
+    def __init__(self):
+        pass
+
+
+v = View()
