9d3a187b001971095e84dcf5555a0ef9e594ebaf
Rutvik Gondekar <gondekarrutvik@gmail.com>
2024-02-18 19:54:38 +0530

    completed ocr funtionality

--- backend/functions.py
+++ backend/functions.py
@@ -6,16 +6,22 @@ from datetime import datetime
 
 
 def format_date(date: str):
-    datetime_obj = datetime.strptime(date, "%Y-%m-%d")
-    return datetime_obj.strftime("%d-%m-%Y")
+    try:
+        datetime_obj = datetime.strptime(date, "%Y-%m-%d")
+        return datetime_obj.strftime("%d-%m-%Y")
+    except TypeError:
+        return None
 
 
 def format_time(time: str):
-    datetime_obj = datetime.strptime(time, "%H:%M")
-    return datetime_obj.strftime("%I:%M %p")
+    try:
+        datetime_obj = datetime.strptime(time, "%H:%M")
+        return datetime_obj.strftime("%I:%M %p")
+    except TypeError:
+        return None
 
 
-def retrieve_taxes(taxes):
+def retrieve_taxes_receipt(taxes):
     tax_list = []
     for tax in taxes:
         tax_list.append({
@@ -31,7 +37,21 @@ def retrieve_taxes(taxes):
         return tax_list
 
 
-def retrieve_line_items(line_items):
+def retrieve_taxes_invoice(taxes):
+    tax_list = []
+    for tax in taxes:
+        tax_list.append({
+            "value": tax.value,
+            "rate": tax.rate
+        })
+
+    if not tax_list:
+        return None
+    else:
+        return tax_list
+
+
+def retrieve_line_items_receipt(line_items):
     line_items_list = []
     for line_item in line_items:
         line_items_list.append({
@@ -39,6 +59,25 @@ def retrieve_line_items(line_items):
             "quantity": line_item.quantity,
             "unit_price": line_item.unit_price,
             "total_amount": line_item.total_amount
+        })
+
+    if not line_items_list:
+        return None
+    else:
+        return line_items_list
+
+
+def retrieve_line_items_invoice(line_items):
+    line_items_list = []
+    for line_item in line_items:
+        line_items_list.append({
+            "product_code": line_item.product_code,
+            "description": line_item.description,
+            "quantity": line_item.quantity,
+            "unit_price": line_item.unit_price,
+            "total_amount": line_item.total_amount,
+            "tax_rate": line_item.tax_rate,
+            "tax_amount": line_item.tax_amount
         })
 
     if not line_items_list:
@@ -59,3 +98,29 @@ def retrieve_registration_info(reg_info):
         return None
     else:
         return reg_info_list
+
+
+def retrieve_reference_number(ref_nums):
+    ref_nums_list = []
+    for ref_num in ref_nums:
+        ref_nums_list.append(ref_num.value)
+    if not ref_nums_list:
+        return None
+    else:
+        return ref_nums_list
+
+
+def retrieve_supplier_payment_details(supplier_payment_details):
+    supplier_payment_details_list = []
+    for detail in supplier_payment_details:
+        supplier_payment_details_list.append({
+            "iban": detail.iban,
+            "swift": detail.swift,
+            "account_number": detail.account_number,
+            "routing_number": detail.routing_number
+        })
+
+    if not supplier_payment_details_list:
+        return None
+    else:
+        return supplier_payment_details_list
--- backend/ocr.py
+++ backend/ocr.py
@@ -13,11 +13,12 @@ class OCR:
         pass
 
     @staticmethod
-    def ocr_receipt(receipt: str):
+    def ocr_receipt(receipt: str) -> dict:
         input_doc = OCR.mindee_client.source_from_path(receipt)
         response: PredictResponse = OCR.mindee_client.parse(product.ReceiptV5, input_doc)
         result = response.document.inference.prediction
-        data = {
+        receipt_data = {
+            "document_type": result.document_type.value,
             "locale": {
                 "country": result.locale.country,
                 "language": result.locale.language,
@@ -29,25 +30,86 @@ class OCR:
             "total_net": result.total_net.value,
             "total_tax": result.total_tax.value,
             "total_amount": result.total_amount.value,
-            "taxes": f.retrieve_taxes(result.taxes),
+            "tip": result.tip.value,
+            "taxes": f.retrieve_taxes_receipt(result.taxes),
             "supplier": {
                 "name": result.supplier_name.raw_value,
                 "reg_info": f.retrieve_registration_info(result.supplier_company_registrations),
                 "address": result.supplier_address.value,
                 "phone_number": result.supplier_phone_number.value
             },
-            "line_items": f.retrieve_line_items(result.line_items)
+            "line_items": f.retrieve_line_items_receipt(result.line_items)
         }
-        return data
+        return receipt_data
 
     @staticmethod
-    def ocr_invoice(invoice):
-        pass
+    def ocr_invoice(invoice: str) -> dict:
+        input_doc = OCR.mindee_client.source_from_path(invoice)
+        response: PredictResponse = OCR.mindee_client.parse(product.InvoiceV4, input_doc)
+        result = response.document.inference.prediction
+        invoice_data = {
+            "document_type": result.document_type.value,
+            "locale": {
+                "language": result.locale.language,
+                "currency": result.locale.currency
+            },
+            "category": result.category.value,
+            "invoice_number": result.invoice_number.value,
+            "reference_numbers": f.retrieve_reference_number(result.reference_numbers),
+            "date": f.format_date(result.date.value),
+            "due_date": f.format_date(result.due_date.value),
+            "total_net": result.total_net.value,
+            "total_tax": result.total_tax.value,
+            "total_amount": result.total_amount.value,
+            "taxes": f.retrieve_taxes_invoice(result.taxes),
+            "supplier": {
+                "payment_details": f.retrieve_supplier_payment_details(result.supplier_payment_details),
+                "name": result.supplier_name.raw_value,
+                "reg_info": f.retrieve_registration_info(result.supplier_company_registrations),
+                "address": result.supplier_address.value
+            },
+            "customer": {
+                "name": result.customer_name.raw_value,
+                "reg_info": f.retrieve_registration_info(result.customer_company_registrations),
+                "address": result.customer_address.value
+            },
+            "line_items": f.retrieve_line_items_invoice(result.line_items)
+        }
+        return invoice_data
 
     @staticmethod
-    def ocr_fin_doc(fin_doc):
-        pass
-
-
-data = OCR.ocr_receipt("../test_data/receipt_7.png")
-print(data)
+    def ocr_fin_doc(fin_doc: str) -> dict:
+        input_doc = OCR.mindee_client.source_from_path(fin_doc)
+        response: PredictResponse = OCR.mindee_client.parse(product.FinancialDocumentV1, input_doc)
+        result = response.document.inference.prediction
+        fin_doc_data = {
+            "document_type": result.document_type.value,
+            "locale": {
+                "language": result.locale.language,
+                "currency": result.locale.currency
+            },
+            "invoice_number": result.invoice_number.value,
+            "reference_numbers": f.retrieve_reference_number(result.reference_numbers),
+            "date": f.format_date(result.date.value),
+            "time": f.format_time(result.time.value),
+            "due_date": f.format_date(result.due_date.value),
+            "total_net": result.total_net.value,
+            "total_tax": result.total_tax.value,
+            "total_amount": result.total_amount.value,
+            "tip": result.tip.value,
+            "taxes": f.retrieve_taxes_receipt(result.taxes),
+            "supplier": {
+                "payment_details": f.retrieve_supplier_payment_details(result.supplier_payment_details),
+                "name": result.supplier_name.raw_value,
+                "reg_info": f.retrieve_registration_info(result.supplier_company_registrations),
+                "address": result.supplier_address.value,
+                "phone_number": result.supplier_phone_number.value
+            },
+            "customer": {
+                "name": result.customer_name.raw_value,
+                "reg_info": f.retrieve_registration_info(result.customer_company_registrations),
+                "address": result.customer_address.value
+            },
+            "line_items": f.retrieve_line_items_invoice(result.line_items)
+        }
+        return fin_doc_data
